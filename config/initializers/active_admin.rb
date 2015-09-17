b27361d6d3f617391d407cf2bae4a14f8d5e5b02
Hung Tran <doanhung.tran@yahoo.com>
2015-09-17 13:07:38 +0700

    Skip default login prompt when visiting admin.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Referral"
+  config.site_title = 'Referral App'
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -55,6 +55,7 @@ ActiveAdmin.setup do |config|
   # This setting changes the method which Active Admin calls
   # within the application controller.
   config.authentication_method = :authenticate_admin_user!
+  config.skip_before_filter :authenticate_organisation!
 
   # == User Authorization
   #
