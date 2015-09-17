d62a998036c94aaaca3cef51cf4ef8406fc64311
Hung Tran <doanhung.tran@yahoo.com>
2015-09-17 12:56:13 +0700

    Exclude admin from locale stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  filter :locale
+  filter :locale, :exclude => /^\/admin/
 
   devise_for :organisations, controllers: { registrations: 'devise/registrations' }
   devise_for :agents, only: :invitations
