aa150f2f7531716ad4d7ba6fa549854e276a634f
Hung Tran <doanhung.tran@yahoo.com>
2015-10-06 13:44:13 +0700

    Fix failing spec.

--- spec/models/agent_spec.rb
+++ spec/models/agent_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Agent, type: :model do
     it { should belong_to :organisation }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
-    it { should validate_presence_of :bank_name }
     it { should validate_inclusion_of(:bank_name).in_array(Agent::BANK_NAMES) }
+    it { should validate_presence_of :insurance_company_name }
     it { should validate_inclusion_of(:insurance_company_name).in_array(Agent::INSURANCE_COMPANIES) }
     it { should validate_presence_of :organisation }
     it { should validate_presence_of :first_name }
